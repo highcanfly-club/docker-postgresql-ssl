0801f12b00b076f9fd8a2045ae08dd1378d08e5a
Ronan <ronan.le_meillat@sctg.eu.org>
2023-02-15 10:10:25 +0000

    more robust detection

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -344,7 +344,9 @@ _main() {
 
 		# only run initialization on an empty data directory
 		#if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
-		if [ ! -f "/var/lib/postgresql/data/INSTALLED" ]; then
+		if [ ! -f "/var/lib/postgresql/data/INSTALLED" ] && [ "$(id -u)" = '0' ]; then
+			#debug
+			ls -lR /var/lib/postgresql
 			docker_verify_minimum_env
 
 			# check dir permissions to reduce likelihood of half-initialized database
